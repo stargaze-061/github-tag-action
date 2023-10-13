e1aff1614ab12309f3962e019a9a800c356e54ad
Pataridze Givi <pataridzegivi@gmail.com>
2023-10-13 16:03:38 -0700

    Renewed

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:18-alpine
-LABEL "repository"="https://github.com/anothrNick/github-tag-action"
-LABEL "homepage"="https://github.com/anothrNick/github-tag-action"
-LABEL "maintainer"="Nick Sjostrom"
+LABEL "repository"="https://github.com/stargaze-061/github-tag-action"
+LABEL "homepage"="https://github.com/stargaze-061/github-tag-action"
+LABEL "maintainer"="Stargaze team"
 
 RUN apk --no-cache add bash git curl jq && npm install -g semver
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,23 +8,14 @@ initial_build_number=${INITIAL_BUILD_NUMBER:-0}
 prerelease_version=${PRERELEASE_VERSION:-true}
 
 # config
-default_semvar_bump=${DEFAULT_BUMP:-minor}
 default_branch=${DEFAULT_BRANCH:-$GITHUB_BASE_REF} # get the default branch from github runner env vars
-with_v=${WITH_V:-false}
-release_branches=${RELEASE_BRANCHES:-master,main}
-custom_tag=${CUSTOM_TAG:-}
 source=${SOURCE:-.}
 dryrun=${DRY_RUN:-false}
 git_api_tagging=${GIT_API_TAGGING:-true}
 initial_version=${INITIAL_VERSION:-0.0.0}
 tag_context=${TAG_CONTEXT:-repo}
-prerelease=${PRERELEASE:-false}
 suffix=${PRERELEASE_SUFFIX:-beta}
 verbose=${VERBOSE:-false}
-major_string_token=${MAJOR_STRING_TOKEN:-#major}
-minor_string_token=${MINOR_STRING_TOKEN:-#minor}
-patch_string_token=${PATCH_STRING_TOKEN:-#patch}
-none_string_token=${NONE_STRING_TOKEN:-#none}
 branch_history=${BRANCH_HISTORY:-compare}
 # since https://github.blog/2022-04-12-git-security-vulnerability-announced/ runner uses?
 git config --global --add safe.directory /github/workspace
@@ -32,24 +23,15 @@ git config --global --add safe.directory /github/workspace
 cd "${GITHUB_WORKSPACE}/${source}" || exit 1
 
 echo "*** CONFIGURATION ***"
-echo -e "\tDEFAULT_BUMP: ${default_semvar_bump}"
 echo -e "\tDEFAULT_BRANCH: ${default_branch}"
-echo -e "\tWITH_V: ${with_v}"
-echo -e "\tRELEASE_BRANCHES: ${release_branches}"
-echo -e "\tCUSTOM_TAG: ${custom_tag}"
 echo -e "\tSOURCE: ${source}"
 echo -e "\tDRY_RUN: ${dryrun}"
 echo -e "\tGIT_API_TAGGING: ${git_api_tagging}"
 echo -e "\tINITIAL_VERSION: ${initial_version}"
 echo -e "\tTAG_CONTEXT: ${tag_context}"
-echo -e "\tPRERELEASE: ${prerelease}"
 echo -e "\tPRERELEASE_SUFFIX: ${suffix}"
 echo -e "\tPRERELEASE_VERSION: ${prerelease_version}"
 echo -e "\tVERBOSE: ${verbose}"
-echo -e "\tMAJOR_STRING_TOKEN: ${major_string_token}"
-echo -e "\tMINOR_STRING_TOKEN: ${minor_string_token}"
-echo -e "\tPATCH_STRING_TOKEN: ${patch_string_token}"
-echo -e "\tNONE_STRING_TOKEN: ${none_string_token}"
 echo -e "\tBRANCH_HISTORY: ${branch_history}"
 echo -e "\tBUILD_NUMBER: ${BUILD_NUMBER}"
 echo -e "\tINITIAL_BUILD_NUMBER: ${initial_build_number}"
@@ -67,63 +49,38 @@ setOutput() {
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 echo "current_branch> $current_branch"
 
-pre_release="$prerelease"
-
-IFS=',' read -ra branch <<< "$release_branches"
-for b in "${branch[@]}"; do
-    # check if ${current_branch} is in ${release_branches} | exact branch match
-    if [[ "$current_branch" == "$b" ]]
-    then
-        pre_release="false"
-    fi
-
-    # verify non specific branch names like  .* release/* if wildcard filter then =~
-    if [ "$b" != "${b//[\[\]|.? +*]/}" ] && [[ "$current_branch" =~ $b ]]
-    then
-        pre_release="false"
-    fi
-done
-echo "pre_release = $pre_release"
-
 # fetch tags
 git fetch --tags
+
+res_count=30
 
 # get the git refs
 git_refs=
 case "$tag_context" in
     *repo*)
-        git_refs=$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)')
+        git_refs=$(git for-each-ref --count="$res_count" --sort=-v:refname --format '%(refname:lstrip=2)')
         ;;
     *branch*)
-        git_refs=$(git tag --list --merged HEAD --sort=-committerdate)
+        git_refs=$(git tag --list --merged HEAD --sort=-committerdate |head -n "$res_count")
         ;;
     * ) echo "Unrecognised context"
         exit 1;;
 esac
 
 # get the latest tag that looks like a semver (with or without v)
-tagFmt="^v?[0-9]+\.[0-9]+\.[0-9]+(\+[0-9]+)?$"
-matching_tag_refs=$( (grep -E "$tagFmt" <<< "$git_refs") || true)
+tagFmt="^v?[0-9]+\.[0-9]+\.[0-9]+(-$suffix(\.[0-9]+)?)(\+([0-9]+))?"
+
+matching_tag_refs=$( (grep -m "$res_count" -E "$tagFmt" <<< "$git_refs") || true)
 tag=$(head -n 1 <<< "$matching_tag_refs")
 
-if $pre_release
-then
-    preTagFmt="^v?[0-9]+\.[0-9]+\.[0-9]+(-$suffix(\.[0-9]+)?)(\+([0-9]+))?$"
-    matching_pre_tag_refs=$( (grep -E "$preTagFmt" <<< "$git_refs") || true)
-    pre_tag=$(head -n 1 <<< "$matching_pre_tag_refs")
-
-    echo "matching_pre_tag_refs> $matching_pre_tag_refs"
-    echo "pre_tag> $pre_tag"
-else
-    echo "matching_tag_refs> $matching_tag_refs"
-    echo "tag> $tag"
-fi
+echo "matching_tag_refs> $matching_tag_refs"
+echo "tag> $tag"
 
 buildNumFmt=".*\+([0-9]+)$"
 latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo "latest_tag> $latest_tag"
 
-if [ "$build_number" = true ]
+if $build_number
 then
     if [[ $latest_tag =~ $buildNumFmt ]]; then current_build_number=${BASH_REMATCH[1]}; fi
 
@@ -142,21 +99,7 @@ fi
 # if there are none, start tags at INITIAL_VERSION
 if [ -z "$tag" ]
 then
-    if $with_v
-    then
-        tag="v$initial_version"
-    else
-        tag="$initial_version"
-    fi
-    if [ -z "$pre_tag" ] && $pre_release
-    then
-        if $with_v
-        then
-            pre_tag="v$initial_version"
-        else
-            pre_tag="$initial_version"
-        fi
-    fi
+    tag="v$initial_version"
 fi
 
 # get current commit hash for tag
@@ -186,104 +129,33 @@ then
     fi
 fi
 
-# get the merge commit message looking for #bumps
-declare -A history_type=(
-    ["last"]="$(git show -s --format=%B)" \
-    ["full"]="$(git log "${default_branch}"..HEAD --format=%B)" \
-    ["compare"]="$(git log "${tag_commit}".."${commit}" --format=%B)" \
-)
-log=${history_type[${branch_history}]}
-printf "History:\n---\n%s\n---\n" "$log"
+new=$(semver "$tag")
 
-case "$log" in
-    *$major_string_token* ) new=$(semver -i major "$tag"); part="major";;
-    *$minor_string_token* ) new=$(semver -i minor "$tag"); part="minor";;
-    *$patch_string_token* ) new=$(semver -i patch "$tag"); part="patch";;
-    *$none_string_token* )
-        echo "Default bump was set to none. Skipping..."
-        setOutput "old_tag" "$tag"
-        setOutput "new_tag" "$tag"
-        setOutput "tag" "$tag"
-        setOutput "part" "$default_semvar_bump"
-        exit 0;;
-    * )
-        if [ "$default_semvar_bump" == "none" ]
-        then
-            echo "Default bump was set to none."
-            new=$(semver "$tag")
-        else
-            new=$(semver -i "${default_semvar_bump}" "$tag")
-            part=$default_semvar_bump
-        fi
-        ;;
-esac
-
-if $pre_release
+# do not want prerelease tag versions   
+if $prerelease_version
 then
-    # get current commit hash for tag
-    pre_tag_commit=$(git rev-list -n 1 "$pre_tag" || true)
-    # skip if there are no new commits for pre_release
-    if [ "$pre_tag_commit" == "$commit" ]
+    # already a pre-release available, bump it
+    if [[ "$tag" =~ $new ]] && [[ "$tag" =~ $suffix ]]
     then
-        echo "No new commits since previous pre_tag. Skipping..."
-        setOutput "new_tag" "$pre_tag"
-        setOutput "tag" "$pre_tag"
-        exit 0
-    fi
-
-    # do not want prerelease tag versions   
-    if [ "$prerelease_version" = false ]
-    then
-        if $with_v
-        then
-            new="v$new-$suffix"
-        else
-            new="$new-$suffix"
-        fi
-        echo -e "Setting ${suffix} pre-tag ${pre_tag} - With pre-tag ${new}"
+        new=v$(semver -i prerelease "${tag}" --preid "${suffix}")
+        echo -e "Bumping ${suffix} pre-tag ${tag}. New pre-tag ${new}"
     else
-        # already a pre-release available, bump it
-        if [[ "$pre_tag" =~ $new ]] && [[ "$pre_tag" =~ $suffix ]]
-        then
-            if $with_v
-            then
-                new=v$(semver -i prerelease "${pre_tag}" --preid "${suffix}")
-            else
-                new=$(semver -i prerelease "${pre_tag}" --preid "${suffix}")
-            fi
-            echo -e "Bumping ${suffix} pre-tag ${pre_tag}. New pre-tag ${new}"
-        else
-            if $with_v
-            then
-                new="v$new-$suffix.0"
-            else
-                new="$new-$suffix.0"
-            fi
-            echo -e "Bumping ${suffix} pre-tag ${pre_tag} - With pre-tag ${new}"
-        fi
+        new="v$new-$suffix.0"
+        echo -e "Bumping ${suffix} pre-tag ${tag} with pre-tag ${new}"
     fi
-
     
-    part="pre-$part"
 else
-    if $with_v
-    then
-        new="v$new"
-    fi
-    echo -e "Bumping tag ${tag} - New tag ${new}"
+    new="v$new"
+    echo -e "Setting ${suffix} pre-tag ${tag} - With pre-tag ${new}"
 fi
+
+part="pre-$part"
 
 # appending build bumber in metadata if needed
 if [ $build_number = true ]
 then
     new=$new'+'$next_build_number
     echo -e "Build number appended to version $new"
-fi
-
-# as defined in readme if CUSTOM_TAG is used any semver calculations are irrelevant.
-if [ -n "$custom_tag" ]
-then
-    new="$custom_tag"
 fi
 
 # set outputs
